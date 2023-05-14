902aa18f08104b620683709a48f156cf2ee2debc
kungfuflex <judoflexchop@gmail.com>
2023-05-14 14:10:21 -0500

    update

--- lib/logger.d.ts
+++ lib/logger.d.ts
@@ -1,3 +1,2 @@
-import { Logger } from "winston";
-export declare const createLogger: (proc?: string) => Logger;
-export declare function getLogger(): Logger;
+export declare const createLogger: (proc?: string) => any;
+export declare function getLogger(): any;
